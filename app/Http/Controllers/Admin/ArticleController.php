1beb6eab91fa4740ef7bbc044367ee7b6ec5d875
朱雁宗 <348977791@qq.com>
2017-04-17 12:34:01 +0800

    修改发送消息成功页面

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -83,10 +83,7 @@ class ArticleController extends Controller
             return redirect()->back()->withInput()->withErrors($response['msg']);
         } else {
             if ( isset($response['errcode']) && $response['errcode']==0 ) {
-                echo "\n\n";
-                var_export($response);
-                echo "\n\n";
-                exit;
+                return view('admin/article/send')->withMediaId($mediaId)->withGroupId($groupId)->withChannelId($this->channelId)->withMsgId($response['msg_id'])->withMsgDataId($response['msg_data_id']);
             } else {
                 return redirect()->back()->withInput()->withErrors($response['errmsg']);
             }
@@ -128,9 +125,6 @@ class ArticleController extends Controller
             ],
             "msgtype" => "mpnews"
         ];
-        echo "\n\n";
-        var_export($postData);
-        echo "\n\n";
         $response = Curl::to($sendUrl)
             ->withData($postData)
             ->asJson()
