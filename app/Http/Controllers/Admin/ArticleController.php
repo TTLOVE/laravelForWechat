4bd0f224d80d7b6aea30a09cb99a479f1e6b75df
zyz <348977791@qq.com>
2019-11-11 14:08:15 +0800

    最新版内容

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -2,14 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
-use App\Article;
-use Ixudra\Curl\Facades\Curl;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Mail;
+use Ixudra\Curl\Facades\Curl;
 
 class ArticleController extends Controller
 {
@@ -18,52 +14,52 @@ class ArticleController extends Controller
     public function __construct()
     {
         $userId = Auth::id();
-        if ( $userId==2 ) {
+        if ($userId == 2) {
             $this->channelId = 1;
         }
     }
 
     /**
-        * 文章列表
-        *
+     * 文章列表
+     *
      */
     public function index()
     {
-        $articleList = $this->getArticleList($this->channelId);
+        $articleList   = $this->getArticleList($this->channelId);
         $userGroupList = $this->getUserGroupList($this->channelId);
-        $pageNum = 1;
-        $pagePre = $pageNum - 1;
-        $pageNext = $pageNum + 1;
+        $pageNum       = 1;
+        $pagePre       = $pageNum - 1;
+        $pageNext      = $pageNum + 1;
         return view('admin/article/index')->withArticles($articleList['item'])->withUserGroup($userGroupList['groups'])->withPagePre($pagePre)->withPageNext($pageNext);
     }
 
     /**
-        * 获取列表
-        *
-        * @param $request
-        *
-        * @return  页面
+     * 获取列表
+     *
+     * @param $request
+     *
+     * @return  页面
      */
     public function pageGet(Request $request)
     {
         $pageNum = $request->get('page');
-        $offset = ($pageNum-1) * 10;
+        $offset  = ($pageNum - 1) * 10;
         // 获取列表
         $userGroupList = $this->getUserGroupList($this->channelId);
-        $articleList = $this->getArticleList($this->channelId, $offset);
+        $articleList   = $this->getArticleList($this->channelId, $offset);
 
-        $pagePre = $pageNum - 1;
+        $pagePre  = $pageNum - 1;
         $pageNext = $pageNum + 1;
         return view('admin/article/index')->withArticles($articleList['item'])->withUserGroup($userGroupList['groups'])->withPagePre($pagePre)->withPageNext($pageNext);
 
     }
 
     /**
-        * 发送消息
-        *
-        * @param $request 请求信息
-        *
-        * @return array
+     * 发送消息
+     *
+     * @param $request 请求信息
+     *
+     * @return array
      */
     public function send(Request $request)
     {
@@ -71,19 +67,19 @@ class ArticleController extends Controller
             'media_id' => 'required',
             'group_id' => 'required',
         ]);
-        $mediaId = $request->get('media_id');
+        $mediaId    = $request->get('media_id');
         $groupIdArr = $request->get('group_id');
 
-        if ( empty($groupIdArr) ) {
+        if (empty($groupIdArr)) {
             return redirect()->back()->withInput()->withErrors('没有选中分组！');
         }
-    
+
         $response = $this->sendMsg($mediaId, $groupIdArr, $this->channelId);
 
-        if ( isset($response['status']) && $response['status']!=1 ) {
+        if (isset($response['status']) && $response['status'] != 1) {
             return redirect()->back()->withInput()->withErrors($response['msg']);
         } else {
-            if ( isset($response['errcode']) && $response['errcode']==0 ) {
+            if (isset($response['errcode']) && $response['errcode'] == 0) {
                 $groupIdString = implode(",", $groupIdArr);
                 return view('admin/article/send')->withMediaId($mediaId)->withGroupId($groupIdString)->withChannelId($this->channelId)->withMsgId($response['msg_id'])->withMsgDataId($response['msg_data_id']);
             } else {
@@ -93,54 +89,48 @@ class ArticleController extends Controller
     }
 
     /**
-        * 根据媒体id,群组id,渠道id发送消息
-        *
-        * @param $mediaId 媒体id
-        * @param $groupIdArr 群组id数组
-        * @param $channelId 渠道id
-        *
-        * @return array
+     * 根据媒体id,群组id,渠道id发送消息
+     *
+     * @param $mediaId 媒体id
+     * @param $groupIdArr 群组id数组
+     * @param $channelId 渠道id
+     *
+     * @return array
      */
     private function sendMsg($mediaId, $groupIdArr, $channelId)
     {
         $wechatToken = $this->getWechatToken($channelId);
-        $returnData = array(
-            'status' => 0,
-            'msg' => '获取失败'
-        );
-        if ( empty($wechatToken) ) {
-            $returnData['status'] = -1; 
-            $returnData['msg'] = "无token，失败！";
-            return $returnData;
+        if (empty($wechatToken)) {
+            throw new Exception("获取 token 失败", 1);
         }
 
         // 发送消息
         $sendUrl = 'https://api.weixin.qq.com/cgi-bin/message/mass/sendall?access_token=' . $wechatToken;
-        if ( in_array('all', $groupIdArr) ) {
+        if (in_array('all', $groupIdArr)) {
             $postData = [
-                "filter" => [
+                "filter"              => [
                     "is_to_all" => true,
-                    "group_id" => 'all'
+                    "group_id"  => 'all',
                 ],
                 "send_ignore_reprint" => 1,
-                "mpnews" => [
-                    "media_id" => $mediaId
+                "mpnews"              => [
+                    "media_id" => $mediaId,
                 ],
-                "msgtype" => "mpnews"
+                "msgtype"             => "mpnews",
             ];
             $response = $this->sendMsgAndEmail($sendUrl, $postData);
         } else {
             foreach ($groupIdArr as $groupId) {
                 $postData = [
-                    "filter" => [
+                    "filter"              => [
                         "is_to_all" => false,
-                        "group_id" => $groupId
+                        "group_id"  => $groupId,
                     ],
                     "send_ignore_reprint" => 1,
-                    "mpnews" => [
-                        "media_id" => $mediaId
+                    "mpnews"              => [
+                        "media_id" => $mediaId,
                     ],
-                    "msgtype" => "mpnews"
+                    "msgtype"             => "mpnews",
                 ];
                 $response = $this->sendMsgAndEmail($sendUrl, $postData);
             }
@@ -150,12 +140,12 @@ class ArticleController extends Controller
     }
 
     /**
-        * 群发消息并发送邮件给我
-        *
-        * @param $sendUrl 发送消息的链接
-        * @param $postData 传输数据
-        *
-        * @return array
+     * 群发消息并发送邮件给我
+     *
+     * @param $sendUrl 发送消息的链接
+     * @param $postData 传输数据
+     *
+     * @return array
      */
     private function sendMsgAndEmail($sendUrl, $postData)
     {
@@ -164,131 +154,111 @@ class ArticleController extends Controller
             ->asJson()
             ->post();
 
-        if ( empty($response) ) {
-            $returnData['status'] = -1; 
-            $returnData['msg'] = "发送失败！";
-            return $returnData;
+        if (empty($response)) {
+            throw new Exception("发送失败", 1);
         }
 
-        $response = json_decode(json_encode($response), true);
-
-        // 获取用户分组信息
-        $groupName = '';
-        $userGroupList = $this->getUserGroupList($this->channelId);
-        foreach ($userGroupList['groups'] as $group) {
-            if ( $group['id']==$postData['filter']['group_id'] ) {
-                $groupName = $group['name'];
-                break;
-            }
-        }
-
-        // 发送163邮件
-        Mail::send('emails.msgMail',['postData'=>$postData, 'response' => $response, 'groupName' => $groupName],function($message){
-            $userId = Auth::id();
-            $userName = '用户' . $userId . '群发消息';
-            $message->from('yanzongnet@163.com', $userName);
-            $to = 'yanzongnet@163.com';
-            $message->to($to)->subject('人工群发消息自动发送邮件');
-        });
-
-        return $response;
+        return json_decode(json_encode($response), true);
     }
 
     /**
-        * 根据渠道id获取对应公众号的用户分组信息
-        *
-        * @param $channelId 渠道id
-        *
-        * @return array
+     * 根据渠道id获取对应公众号的用户分组信息
+     *
+     * @param $channelId 渠道id
+     *
+     * @return array
      */
     private function getUserGroupList($channelId)
     {
         $wechatToken = $this->getWechatToken($channelId);
-        $returnData = array(
-            'status' => 0,
-            'msg' => '获取失败'
-        );
-        if ( empty($wechatToken) ) {
-            $returnData['status'] = -1; 
-            $returnData['msg'] = "无token，失败！";
-            return $returnData;
+        if (empty($wechatToken)) {
+            throw new Exception("获取 token 失败", 1);
         }
 
         // 获取列表
-        $listUrl = 'https://api.weixin.qq.com/cgi-bin/groups/get?access_token=' . $wechatToken;
+        $listUrl  = 'https://api.weixin.qq.com/cgi-bin/groups/get?access_token=' . $wechatToken;
         $response = Curl::to($listUrl)
             ->get();
-        if ( empty($response) ) {
-            echo "无分组";
-            return false;
+        if (empty($response)) {
+            throw new Exception("获取  分组信息 失败", 1);
         }
-        $response = json_decode($response, true);
+        $response     = json_decode($response, true);
         $allSendGroup = [
-            'id' => 'all',
-            'name' => '全部发送',
-            'count' => 0
+            'id'    => 'all',
+            'name'  => '全部发送',
+            'count' => 0,
         ];
         array_unshift($response['groups'], $allSendGroup);
         return $response;
     }
 
     /**
-        * 根据渠道id获取文章列表
-        *
-        * @param $channelId 渠道id
-        * @param $offset 查询开始数
-        * @param $limit 每次读取数量
-        *
-        * @return array
+     * 根据渠道id获取文章列表
+     *
+     * @param $channelId 渠道id
+     * @param $offset 查询开始数
+     * @param $limit 每次读取数量
+     *
+     * @return array
      */
-    private function getArticleList($channelId, $offset=0, $limit=10)
+    private function getArticleList($channelId, $offset = 0, $limit = 10)
     {
         $wechatToken = $this->getWechatToken($channelId);
-        $returnData = array(
-            'status' => 0,
-            'msg' => '获取失败'
-        );
-        if ( empty($wechatToken) ) {
-            $returnData['status'] = -1; 
-            $returnData['msg'] = "无token，失败！";
-            return $returnData;
+        if (empty($wechatToken)) {
+            throw new Exception("获取 token 失败", 1);
         }
 
         // 获取列表
-        $listUrl = 'https://api.weixin.qq.com/cgi-bin/material/batchget_material?access_token=' . $wechatToken;
+        $listUrl  = 'https://api.weixin.qq.com/cgi-bin/material/batchget_material?access_token=' . $wechatToken;
         $postData = [
-            'type' => 'news',
+            'type'   => 'news',
             'offset' => $offset,
-            'count' => $limit
+            'count'  => $limit,
         ];
         $response = Curl::to($listUrl)
             ->withData($postData)
             ->asJson()
             ->post();
-        if ( empty($response) ) {
-            echo "无文章";
-            return false;
+        if (empty($response)) {
+            throw new Exception("获取 文章列表 失败", 1);
         }
         $response = json_decode(json_encode($response), true);
         return $response;
     }
 
     /**
-        * 获取对应渠道access_token
-        *
-        * @param $channelId 渠道id
-        *
-        * @return string
+     * 获取对应渠道access_token
+     *
+     * @param $channelId 渠道id
+     *
+     * @return string
      */
     private function getWechatToken($channelId)
     {
-        $response = Curl::to('https://tpassport.lifeq.com.cn/pass/auth/channel/token')
-            ->withData( array( 'channelId' => $channelId ) )
-            ->get();
-        if ( empty($response) ) {
-            return '';
+        $token = \DB::table('tokens')->where('expire_time', '>', time())
+            ->orderBy('id', 'desc')
+            ->first();
+
+        if (empty($token)) {
+            $response = Curl::to('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx987894680b1d2f52&secret=ed85577df79c9c6003702b220f9a202a')
+                ->get();
+            $response = @json_decode($response, true);
+            if (empty($response) || !isset($response['access_token'])) {
+                throw new Exception("获取token 失败", 1);
+            }
+
+            // 插入信息
+            $token = [
+                'token'       => $response['access_token'],
+                'expire_time' => time() + 7000,
+            ];
+            \DB::table('tokens')->insert($token);
+
+            $token = \DB::table('tokens')->where('expire_time', '>', time())
+                ->orderBy('id', 'desc')
+                ->first();
         }
-        $response = json_decode($response, true);
-        return isset($response['response']['access_token']) ? $response['response']['access_token'] : '';
+
+        return $token->token;
     }
 }
